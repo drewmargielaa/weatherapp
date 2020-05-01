4adbb9dce2103100b685375a19023715addded3e
Andrew Souza <drewmargielaa@gmail.com>
2020-05-01 19:46:04 -0400

    fixed api call

--- script.js
+++ script.js
@@ -1,107 +1,51 @@
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for openweather API based on form inputs
- */
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=321f01b5370831c8a95c71d93ecaa99f{}";
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "321f01b5370831c8a95c71d93ecaa99f" };
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
-    
-    // Logging the URL so we have access to it for troubleshooting
-    console.log("---------------\nURL: " + queryURL + "\n---------------");
-    console.log(queryURL + $.param(queryParams));
-    return queryURL + $.param(queryParams);
-  }
-  /**
-   * takes API data (JSON/object) and turns it into elements on the page
-   * @param {object} weatherdata - object containing openweather API data
-   */
-  function updatePage(weatherdata) {
-    // Get from the form the number of results to display
-    // API doesn't have a "limit" parameter, so we have to do this ourselves
-    var weatherCount = $("#weather-count").val();
-    // Log the weatherdata to console, where it will show up as an object
-    console.log(weatherdata);
-    console.log("------------------------------------");
-    // Loop through and build elements for the defined number of articles
-    for (var i = 0; i < weatherCount; i++) {
-      // Get specific weather info for current index
-      var weather = weatherdata.response.docs[i];
-      // Increase the weatherCount (track article # - starting at 1)
-      var weathers = i + 1;
-      // Create the  list group to contain the weather and add the article content for each
-      var $weatherList = $("<ul>");
-      $weatherList.addClass("list-group");
-      // Add the newly created element to the DOM
-      $("#weather-results").append($weatherList);
-      // If the article has a headline, log and append to $articleList
-      var headline = article.headline;
-      var $articleListItem = $("<li class='list-group-item articleHeadline'>");
-      if (headline && headline.main) {
-        console.log(headline.main);
-        $articleListItem.append(
-          "<span class='label label-primary'>" +
-            articleCount +
-            "</span>" +
-            "<strong> " +
-            headline.main +
-            "</strong>"
-        );
-      }
-      // If the article has a byline, log and append to $articleList
-      var byline = article.byline;
-      if (byline && byline.original) {
-        console.log(byline.original);
-        $articleListItem.append("<h5>" + byline.original + "</h5>");
-      }
-      // Log section, and append to document if exists
-      var section = article.section_name;
-      console.log(article.section_name);
-      if (section) {
-        $articleListItem.append("<h5>Section: " + section + "</h5>");
-      }
-      // Log published date, and append to document if exists
-      var pubDate = article.pub_date;
-      console.log(article.pub_date);
-      if (pubDate) {
-        $articleListItem.append("<h5>" + article.pub_date + "</h5>");
-      }
-      // Append and log url
-      $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
-      console.log(article.web_url);
-      // Append the article
-      $articleList.append($articleListItem);
-    }
-  }
-  // Function to empty out the articles
-  function clear() {
-    $("#weather-results").empty();
-  }
-  // CLICK HANDLERS
-  // ==========================================================
-  // .on("click") function associated with the Search Button
-  $("#run-search").on("click", function(event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
-    event.preventDefault();
-    // Empty the region associated with the articles
-    clear();
-    // Build the query URL for the ajax request to the NYT API
-    var queryURL = buildQueryURL();
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
+function getUV(lat,lon){
+  var uvQueryURL = `https://api.openweathermap.org/data/2.5/uvi?appid=321f01b5370831c8a95c71d93ecaa99f&lat=${lat}&lon=${lon}`;
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(updatePage);
+    url: uvQueryURL,
+    method: "GET"
+  }).then(function (uvData) {
+    let uvResult =  uvData;
+    $("#weather-results").append("<p> UV: " + uvData.value + "</p>")
   });
-  //  .on("click") function associated with the clear button
-  $("#clear-all").on("click", clear);
-  
+}
+function clear() {
+  $("#weather-results").empty();
+}
+$("#run-search").on("click", function (event) {
+  event.preventDefault();
+
+  clear();
+  // Build the query URL for the ajax request to the NYT API
+  var searchCity = $("#search-term")
+    .val()
+    .trim();
+  // queryURL is the url we'll use to query the API
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=321f01b5370831c8a95c71d93ecaa99f`;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (result) {
+    console.log(result)
+    $("#weather-results").append("<p> Temp: " + result.main.temp + "</p>")
+    $("#weather-results").append("<p> Wind Speed: " + result.wind.speed + "</p>")
+    getUV(result.coord.lat, result.coord.lon)
+
+    var queryFiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity}&appid=321f01b5370831c8a95c71d93ecaa99f`;
+    $.ajax({
+      url: queryFiveDayURL,
+      method: "GET"
+    }).then(function (fiveDayResult) {
+      console.log(fiveDayResult)
+      let currentDay = 0;
+      for (let i = 0; i < fiveDayResult.list.length; i = i +7 ){
+        currentDay++;
+        console.log(fiveDayResult.list[i])
+        $("#weather-results").append(`<p> Temp ${currentDay+1}: ${fiveDayResult.list[i].main.temp}</p>`)
+        $("#weather-results").append(`<p> Wind Speed ${currentDay+1}: ${fiveDayResult.list[i].wind.speed}</p>`)
+  }
+     });
+        });
+});
+//  .on("click") function associated with the clear button
+$("#clear-all").on("click", clear);
